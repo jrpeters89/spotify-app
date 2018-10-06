4d311b06d6614d4023d74dd07b2fdb11e094c458
James Peters <james@teambuildr.com>
2018-10-06 00:01:50 -0400

    First part of Spotify project

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Playlist.css';
+import TrackList from '../TrackList/TrackList';
+
+class Playlist extends Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="Playlist">
+        <input defaultValue={this.props.name} onChange={this.handleNameChange}/>
+        <TrackList tracks={this.props.tracks} onRemove={this.props.removeTrack} isRemoval={true} />
+        <a className="Playlist-save">SAVE TO SPOTIFY</a>
+      </div>
+    )
+  }
+};
+
+export default Playlist;
